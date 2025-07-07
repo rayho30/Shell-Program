8a400303d975517b1d42d90d1f0d225359e0c05b
Rayhan Islam Prome <rayhanspydet@gmail.com>
2025-07-07 22:36:01 +0600

    Add files via upload

---
+++ mathmatical.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter the first number:"
+read num1
+echo "Enter the second number:"
+read num2
+echo "Sum: $((num1 + num2))"
+echo "Difference: $((num1 - num2))"
+echo "Product: $((num1 * num2))"
+echo "Quotient: $((num1 / num2))"
