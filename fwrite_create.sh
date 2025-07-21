0d4e0318c91d4e38ce85845a2dd813e541c0bc89
Rayhan Islam Prome <rayhanspydet@gmail.com>
2025-07-21 22:12:33 +0600

    Add files via upload

---
+++ fwrite_create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Enter the file name: " file
+echo "Enter text to write:"
+read text
+echo "$text" > "$file"
+echo "-----------------------------------"
+echo "The File $file is created!"
